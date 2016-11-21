702f1b6dca7c1d4d4be5bc1ed6544c1feb059af6
Mark Taylor <moderngeekish@gmail.com>
2016-11-21 22:47:06 +0000

    Fixed a parse issue
    Fixed an issue where the arguments were not correctly built for empty commands

--- oh-my-gosh.go
+++ oh-my-gosh.go
@@ -21,10 +21,17 @@ func main() {
 }
 
 func parseCommand(command string) {
-	commands := strings.Fields(command)
-	command = commands[0]
-	args := commands[1:]
+	if command == "" {
+		//Do nothing
+	} else {
+		commands := strings.Fields(command)
+		command = commands[0]
+		args := commands[1:]
+		runCommand(command, args...)
+	}
+}
 
+func runCommand(command string, args ...string) {
 	if command == "exit" {
 		os.Exit(1)
 	} else if command == "cd" {
